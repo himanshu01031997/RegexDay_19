ac3f33d3e289ea78da83f312f467e320ed845382
himanshu01031997 <hs010397@gmail.com>
2022-10-24 15:50:18 +0530

    add: valid email

--- Pattern.cs
+++ Pattern.cs
@@ -8,12 +8,12 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace RegexDemo
 {
-    public  class Pattern
+    public class Pattern
     {
-        public static string REGEX_NAME = "^[A-Z][a-z]{2,15}";
-        public  bool ValidateNamecode(string NAME)
+        public static string REGEX_Email = "^[A-Za-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}$";
+        public bool ValidateNamecode(string email)
         {
-            return Regex.IsMatch(NAME, REGEX_NAME);
+            return Regex.IsMatch(email, REGEX_Email);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@
         static void Main(string[] args)
         {
             Pattern p = new Pattern();
-            Console.WriteLine("Enter The Last name:");
-            string Lastname=Console.ReadLine();
-            bool value= p.ValidateNamecode(Lastname);
+            Console.WriteLine("Enter The Emailid:");
+            string Emailid = Console.ReadLine();
+            bool value= p.ValidateNamecode(Emailid);
             if (value)
             {
-                Console.WriteLine("LastName is Correct");
+                Console.WriteLine("Emailid is Correct");
             }
             else
             {
-                Console.WriteLine("LastName is not Correct");
+                Console.WriteLine("Emailid is not Correct");
             }
             
         }
