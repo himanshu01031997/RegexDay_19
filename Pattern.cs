204eefa7b7acfd2a02b7d8a444f2420e4a2a8596
himanshu01031997 <hs010397@gmail.com>
2022-10-24 15:34:22 +0530

    add:valid user name

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RegexDemo
+{
+    public  class Pattern
+    {
+        public static string REGEX_NAME = "^[A-Z][a-zA-Z]{2,15}";
+        public  bool ValidateNamecode(string NAME)
+        {
+            return Regex.IsMatch(NAME, REGEX_NAME);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RegexDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern p = new Pattern();
+            Console.WriteLine("Enter The first name:");
+            string firstname=Console.ReadLine();
+            bool value= p.ValidateNamecode(firstname);
+            if (value)
+            {
+                Console.WriteLine("Name is Correct");
+            }
+            else
+            {
+                Console.WriteLine("Name is not Correct");
+            }
+            
+        }
+    }
+}
